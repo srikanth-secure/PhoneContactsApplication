4ce7e982156e1657c857bffe517420ab9d04fe97
sri <sri@gmail.com>
2020-09-18 22:39:18 +1000

    added .yml file and implemented logger in two controllers

--- src/main/java/com/srikanth/controller/ContactInfoController.java
+++ src/main/java/com/srikanth/controller/ContactInfoController.java
@@ -2,6 +2,8 @@ package com.srikanth.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,35 +19,66 @@ import com.srikanth.service.ContactService;
 @Controller
 public class ContactInfoController {
 
+	private static final Logger logger = LoggerFactory.getLogger(ContactInfoController.class);
+
 	@Autowired
 	private ContactService contactService;
 
 	@GetMapping(value = { "/", "loadForm" })
 	public String index(Model model) {
+
+		logger.debug("   ***   index() method execution is started for display contact form   ***");
+
 		ContactEntity contactDtlsEntity = new ContactEntity();
 		model.addAttribute("contact", contactDtlsEntity);
+
+		logger.debug("   ***   index() method execution is ended for display contact form   ***");
+
+		logger.info("   ***   index() method executed successfully for display contact form   ***");
+
 		return "index";
 	}
 
 	@PostMapping("/saveContact")
 	public String handleSubmitBtn(@ModelAttribute("contact") Contact c, RedirectAttributes reModel) {
+
+		logger.info("   ***   handleSubmitBtn() method exceution is started***");
+
 		boolean isSaved = contactService.saveContact(c);
 		if (isSaved) {
 			if (c.getConId() != null) {
 				reModel.addFlashAttribute("succMsg", "Contact Updated Successfully");
+
+				logger.info("   ***   in handleSubmitBtn() method contact updated***");
+
 			} else {
+
+				logger.info("   ***   handleSubmitBtn() method contact saved***");
+
 				reModel.addFlashAttribute("succMsg", "Contact Saved Successfully");
 			}
 		} else {
+
+			logger.info("   ***   handleSubmitBtn() method contact not saved***");
+
 			reModel.addFlashAttribute("errMsg", "Failed to Save Contact");
 		}
+
+		logger.info("   ***   handleSubmitBtn() method exceution is finished***");
+
 		return "redirect:/loadForm";
 	}
 
 	@GetMapping("/viewContacts")
 	public String handleViewLink(Model model) {
+
+		logger.info("   ***   handleViewLink() method exceution is started***");
+
 		List<Contact> allContacts = contactService.getAllContacts();
 		model.addAttribute("contacts", allContacts);
+
+		logger.info("   ***   handleViewLink() method exceution is finished***");
+
 		return "viewContacts";
 	}
 }
--- src/main/java/com/srikanth/controller/ViewContactsController.java
+++ src/main/java/com/srikanth/controller/ViewContactsController.java
@@ -1,5 +1,7 @@
 package com.srikanth.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,24 +15,44 @@ import com.srikanth.service.ContactService;
 @Controller
 public class ViewContactsController {
 
+	private static final Logger logger = LoggerFactory.getLogger(ContactInfoController.class);
+
 	@Autowired
 	private ContactService contactService;
 
 	@GetMapping("/editContact")
 	public String editContact(@RequestParam("conId") Integer conId, Model model) {
+
+		logger.info("   ***   editContact() method exceution is started***");
+
 		Contact contact = contactService.getContactById(conId);
 		model.addAttribute("contact", contact);
+
+		logger.info("   ***   editContact() method exceution is finished***");
+
 		return "index";
 	}
 
 	@GetMapping("/deleteContact")
 	public String deleteContact(@RequestParam("conId") Integer conId, RedirectAttributes reModel) {
+
+		logger.info("   ***   deleteContact() method exceution is started***");
+
 		boolean isDeleted = contactService.deleteContact(conId);
 		if (isDeleted) {
+
+			logger.info("   ***   in deleteContact() method, contact deleted Successfully***");
+
 			reModel.addFlashAttribute("succMsg", "Contact Deleted Successfully");
 		} else {
+
+			logger.info("   ***   in deleteContact() method, contact deletion failed***");
+
 			reModel.addFlashAttribute("errMsg", "Failed to Delete Contact");
 		}
+
+		logger.info("   ***   deleteContact() method exceution is finished***");
+
 		return "redirect:viewContacts";
 	}
 }
